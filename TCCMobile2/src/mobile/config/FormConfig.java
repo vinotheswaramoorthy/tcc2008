8680dc9407d1f726ae1f072a3f96903c2df230e3
dev.ivan <dev.ivan@001f843a-8347-11dd-88bd-cff828a63736>
2008-11-13 20:11:52 +0000

    Incluido os campos para printscreen

--- TCCMobile2/src/mobile/config/FormConfig.java
+++ TCCMobile2/src/mobile/config/FormConfig.java
@@ -1,21 +1,75 @@
 package mobile.config;
 
+import javax.microedition.lcdui.Displayable;
+
 import mobile.ui.BaseForm;
 
+import com.sun.lwuit.Component;
+import com.sun.lwuit.Container;
+import com.sun.lwuit.Display;
 import com.sun.lwuit.Form;
 import com.sun.lwuit.Label;
+import com.sun.lwuit.RadioButton;
+import com.sun.lwuit.TextArea;
+import com.sun.lwuit.TextField;
 import com.sun.lwuit.events.ActionEvent;
 import com.sun.lwuit.events.ActionListener;
 import com.sun.lwuit.layouts.BorderLayout;
+import com.sun.lwuit.layouts.BoxLayout;
+import com.sun.lwuit.layouts.GridLayout;
+import com.sun.lwuit.layouts.Layout;
+import com.sun.lwuit.plaf.UIManager;
 
 public class FormConfig extends BaseForm{
 
 	protected void execute(Form f) {
 
+		f.setTitle("Configurações");
+		
 		f.setLayout(new BorderLayout());
+		f.setScrollable(false);
+		//f.addComponent(BorderLayout.CENTER,new Label("Configurações"));
 		
-		f.addComponent(BorderLayout.CENTER,new Label("Configurações"));
+		Container cntList = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+		cntList.setScrollable(true);
+		cntList.addComponent(getConfigItem("Nome"));
+		cntList.addComponent(getConfigItem("Apelido"));
+		cntList.addComponent(getConfigItem("Foto"));
 		
+		Label lblSexo = new Label("Sexo");
+		lblSexo.getStyle().setBgTransparency(0);		
+		cntList.addComponent(lblSexo);
+		
+		RadioButton rbM = new RadioButton("Masculino");
+		rbM.getStyle().setBgTransparency(0);
+		cntList.addComponent(rbM);
+		
+		RadioButton rbF = new RadioButton("Feminino");
+		rbF.getStyle().setBgTransparency(0);
+		cntList.addComponent(rbF);
+		
+		Label lblDesc = new Label("Descrição");
+		lblDesc.getStyle().setBgTransparency(0);
+		cntList.addComponent(lblDesc);
+		int max = Display.getInstance().getDisplayWidth();
+		cntList.addComponent(new TextArea( String.valueOf( 10 ), 4, max/10 ));
+		
+		
+		
+		f.addComponent(BorderLayout.CENTER,cntList);
+	}
+	
+	private Component getConfigItem(String label){
+	
+		Container cnt = new Container(new BorderLayout());
+		Label lbl = new Label(label);	
+		lbl.getStyle().setBgTransparency(0);
+		cnt.addComponent( BorderLayout.NORTH, lbl);
+		int cols = (Display.getInstance().getDisplayWidth() - lbl.getWidth()) /11;
+		TextField txt = new TextField(cols);
+		cnt.addComponent( BorderLayout.CENTER, txt);
+		
+		return cnt;
 	}
 
 	public String getName() {
